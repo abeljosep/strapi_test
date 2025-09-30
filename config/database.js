0fcd45663b2ec9811cb7f48ebef6a49f54999eb3
abeljosepm <abeljosepm@gmail.com>
2025-09-30 16:23:36 -0400

    added public schema

--- config/database.js
+++ config/database.js
@@ -18,6 +18,7 @@ module.exports = ({ env }) => ({
       user: env('DB_USER'),
       password: env('DB_PASS'),
       ssl: true,
+      schema: env('DB_SCHEMA', "public"),
     },
     debug: false,
   }
